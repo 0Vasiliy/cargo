044e76a19e3bcc2ac58f128053f15e82296f0c6e
Козин-Поздняков <vasiliy_pit@mail.ru>
2024-12-15 01:24:43 +0300

    corected base

--- src/stores/CargosStore.js
+++ src/stores/CargosStore.js
@@ -4,14 +4,12 @@ import axios from 'axios'
 export const useCargosStore = defineStore('cargosStore',{
 state: () => ({
   cargoList: [],
-  // statusList: [],
 }),
 actions:{
   fetchCargos(){
-        axios.get('../../db.json') // local ../../db.json deploy github ../db.json
+        axios.get('../../db.json')
         .then(response => {
              this.cargoList = response.data;
-            //  this.statusList = response.data.status; 
             })
         .catch(error => {
              console.log(error); 
